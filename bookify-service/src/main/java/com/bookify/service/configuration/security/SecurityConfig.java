75bed0afbe7e7ded8bedf50719ff2a3c441f435e
Matija Kokor <matijakokorr@gmail.com>
2024-12-29 20:20:32 +0100

    Revert "Enable CORS"
    This reverts commit b5e720d08be7990d3026db1312a64e9aa9f145ee.

--- bookify-service/src/main/java/com/bookify/service/configuration/security/SecurityConfig.java
+++ bookify-service/src/main/java/com/bookify/service/configuration/security/SecurityConfig.java
@@ -1,30 +1,31 @@
 package com.bookify.service.configuration.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import com.bookify.api.enums.Role;
 import com.bookify.service.filter.AccessTokenAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
   private final AccessTokenAuthenticationFilter accessTokenAuthenticationFilter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
-    http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+    http.cors(withDefaults())
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(req ->
             req.requestMatchers("/book/authorization-employee")
@@ -38,20 +39,6 @@ public class SecurityConfig {
             UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
-  }
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true);
-    config.addAllowedOrigin("*");
-    config.addAllowedHeader("*");
-    config.addAllowedMethod("*");
-    config.setMaxAge(3600L);
-
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", config);
-    return source;
   }
 
 }
